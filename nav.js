9c083343c8579c3485f22a53dea6a96f7faea0f8
hokole <94093120+hokole@users.noreply.github.com>
2023-06-15 23:18:35 -0500

    Made mobile friendly version and fixed bugs

--- nav.js
+++ nav.js
@@ -1,28 +1,33 @@
-// var prevScrollpos = window.pageYOffset;
-// window.onscroll = function() {
-//   var currentScrollPos = window.pageYOffset;
-//   if (prevScrollpos > currentScrollPos) {
-//     document.getElementById("navbar").style.top = "0";
-
-//     document.getElementById("logo").style.top = "15px";
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
 
     
-//   } else {
-//     document.getElementById("navbar").style.top = "-100px";
-//     if (window.screen.width <= 600) {
-//       document.getElementById("logo").style.top = "15px";
-//     } else {
-//     document.getElementById("logo").style.top = "125px";
-//     }
-//   }
-//   prevScrollpos = currentScrollPos;
-// }
+
+    
+  } else {
+    document.getElementById("navbar").style.top = "-500px";
+    
+  }
+  prevScrollpos = currentScrollPos;
+}
 
 function myFunction() {
   var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
+  var y = document.getElementById("navbar");
+  if (x.className === "topnav" || y.className === "headerNav") {
     x.className += " responsive";
+    y.className += " responsive";
   } else {
     x.className = "topnav";
+    y.className = "headerNav";
   }
+
+  // if (y.className === "headerNav") {
+  //   y.className += " responsive";
+  // } else {
+  //   y.className = "headerNav";
+  // }
 }
