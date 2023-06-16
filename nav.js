1f96645e9b183a726813e311b6c70210e55a5c83
hokole <94093120+hokole@users.noreply.github.com>
2023-06-15 21:12:49 -0500

    Converted to mobile friendly

--- nav.js
+++ nav.js
@@ -1,13 +1,28 @@
-var prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
-  var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    document.getElementById("navbar").style.top = "0";
-    document.getElementById("logo").style.top = "15px";
+// var prevScrollpos = window.pageYOffset;
+// window.onscroll = function() {
+//   var currentScrollPos = window.pageYOffset;
+//   if (prevScrollpos > currentScrollPos) {
+//     document.getElementById("navbar").style.top = "0";
+
+//     document.getElementById("logo").style.top = "15px";
+
     
+//   } else {
+//     document.getElementById("navbar").style.top = "-100px";
+//     if (window.screen.width <= 600) {
+//       document.getElementById("logo").style.top = "15px";
+//     } else {
+//     document.getElementById("logo").style.top = "125px";
+//     }
+//   }
+//   prevScrollpos = currentScrollPos;
+// }
+
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
   } else {
-    document.getElementById("navbar").style.top = "-100px";
-    document.getElementById("logo").style.top = "125px";
+    x.className = "topnav";
   }
-  prevScrollpos = currentScrollPos;
 }
